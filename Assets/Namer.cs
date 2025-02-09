1d3587f8fd970fabe780db8793088f589ba25d48
Kreis <kreisburgergames@gmail.com>
2025-02-08 21:14:06 -0500

    namer number

--- Assets/Namer.cs
+++ Assets/Namer.cs
@@ -25,11 +25,26 @@ public class Namer : MonoBehaviour
         transform.parent = null;
     }
 
+    private int GetPlayerNumber()
+    {
+        int num = 1;
+        foreach(Photon.Realtime.Player player in PhotonNetwork.PlayerList)
+        {
+            if(player == PhotonNetwork.LocalPlayer)
+            {
+                break;
+            }
+            num++;
+        }
+
+        return num;
+    }
+
     public string CheckName()
     {
         if(currentName == "" || currentName == null)
         {
-            return "Player " + PhotonNetwork.LocalPlayer.UserId[0];
+            return "Player " + GetPlayerNumber();
         }
         return currentName;
     }
